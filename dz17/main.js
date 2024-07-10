f14b719f5a9b5a10bc0de47607bddb375e8e5228
strangespirit <anfisakireevaa@gmail.com>
2024-07-10 13:45:14 +0300

    add dz17

---
+++ dz17/main.js
@@ -0,0 +1,23 @@
+for (let i = 2; i < 13; i++) {
+    if (i%2==0) {
+        console.log(i);
+    }
+}
+
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+}
+
+let i = 2
+while (i < 13) {
+    if (i%2==0) {
+        console.log(i);
+    }
+    i++;
+}
+
+let j = 10
+while (j > 0) {
+    console.log(j);
+    j--
+}
