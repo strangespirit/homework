e59b745f33a446b88bb4cad7197ae6906e8756cc
strangespirit <anfisakireevaa@gmail.com>
2024-07-10 13:44:52 +0300

    add dz16

---
+++ dz16/main.js
@@ -0,0 +1,8 @@
+let age = 19
+if (age >= 18) {
+    console.log('Вы совершеннолетний.')
+} else {
+    console.log('Вам еще нет 18.')
+}
+
+(age >= 18) ? console.log('Вы совершеннолетний.') : console.log('Вам еще нет 18.')
