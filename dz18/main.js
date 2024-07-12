e810d0deb584374a459e4c6089996a4109a91d7f
strangespirit <anfisakireevaa@gmail.com>
2024-07-12 21:58:32 +0300

    add dz18

---
+++ dz18/main.js
@@ -0,0 +1,11 @@
+let arr = [10, 6, 122, 15, 84, 45, 999]
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i]%2==0) {
+        console.log(arr[i])
+    }
+}
+
+let rainbow = ['красный', 'оранжевый', 'желтый', 'зеленый', 'голубой', 'синий', 'фиолетовый']
+for (let i = rainbow.length - 1; i >= 0; i--) {
+    console.log(rainbow[i])
+}
